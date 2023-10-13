bd43f270e7e648b77381dd8458284aaea999bb4d
Rachael Orben <raorben@gmail.com>
2023-10-13 11:16:38 -0300

    maps & deployment duration

--- GPSviz_1b_ProjectMaps.R
+++ GPSviz_1b_ProjectMaps.R
@@ -1,15 +1,17 @@
 library(dplyr)
 library(lubridate)
 library(ggplot2)
-library(data.table) #rename
 library(stringr)
 library(R.utils)
 library(tidyr)
-library(zoo)#dive phase
-library(pracma)#matlab style gradient function
-library(argosfilter)#forward-backward speed filter for GPS data
 library(sf)
 library(MetBrewer)
+
+library(ggmap)
+library(osmdata)
+library(argosfilter)
+library(ggpubr)
+
 
 if(Sys.info()[7]=="rachaelorben") {
   usrdir<-"/Users/rachaelorben/Library/CloudStorage/Box-Box/DASHCAMS/"
@@ -19,8 +21,21 @@ if(Sys.info()[7]=="rachaelorben") {
   source('/Users/rachaelorben/git_repos/CormOcean/MakeDive.R')
 }
 
+#  Deployment matrix ---------------------------------------------
+deploy_matrix<-read.csv(paste0(usrdir,deplymatrix))
+deploy_matrix$DeploymentStartDatetime<-mdy_hm(deploy_matrix$DeploymentStartDatetime)-(deploy_matrix$UTC_offset_deploy*60*60)
+deploy_matrix$DeploymentEndDatetime_UTC<-mdy_hm(deploy_matrix$DeploymentEndDatetime_UTC)
+dm<-deploy_matrix%>%select(Bird_ID,TagSerialNumber,Project_ID,DeploymentStartDatetime,Deployment_End_Short,DeploymentEndDatetime_UTC,TagManufacture)%>%
+  filter(is.na(TagSerialNumber)==FALSE)
+
+#all project names
+prjt<-unique(deploy_matrix$Project_ID)
+rm(deploy_matrix)
+
 # Map Check --------------------------------------------------------------------
 # Pulls in saved data and plots
+
+prjt
 
 for (i in 1:length(prjt)){
   
@@ -36,7 +51,6 @@ for (i in 1:length(prjt)){
   x_min<-min(locs$lon)-.25
   x_max<-max(locs$lon)+.25
   
-  dt<-Sys.Date()
   ids<-unique(locs$Uni_ID)
   
   #quartz()
@@ -51,8 +65,27 @@ for (i in 1:length(prjt)){
     theme(legend.title=element_blank(),
           legend.text = element_blank())+
     guides(colour = guide_legend(override.aes = list(size=3)))
-  ggsave(paste0(usrdir,savedir,"PLOTS/DeploymentMaps/",prjt[i],"_",dt,"_Map.png"), dpi=300)
+  ggsave(paste0(usrdir,savedir,"PLOTS/DeploymentMaps/",prjt[i],"_Map.png"), dpi=300)
   
+ # #Open Street Map Version: FUNCTIONAL SORTA - NEEDS WORK
+ #  (map <- get_map(c(left = x_min, bottom = y_min, right = x_max, top = y_max),source="stamen"))
+ #  
+ #  temp_plot<-
+ #    #geom_polygon(data=w2hr,aes(long,lat,group=group),fill="grey70",color="grey60",linewidth=0.1)+
+ #    ggmap(map)+
+ #    geom_path(data=locs,aes(x=lon,y=lat, group=device_id))+
+ #    geom_point(data=locs,aes(x=lon,y=lat, color=device_id), size=.5)+
+ #    xlab("Longitude")+
+ #    ylab("Latitude")+
+ #    coord_fixed(ratio=1.7,xlim = c(x_min,x_max),ylim=c(y_min,y_max))+
+ #    #labs(title=tit)+
+ #    theme_bw()+
+ #    theme(legend.title=element_blank(),
+ #          legend.text = element_blank())+
+ #    guides(colour = guide_legend(override.aes = list(size=3)))
+ #  ggsave(temp_plot,filename = paste0(usrdir,savedir,"PLOTS/DeploymentMaps/",prjt[i],"_OpenSMap.png"),height=4,width=8,device = "png")
+ #  
+ #  
   # All Bird Data Coverage --------------------------------------------------
   locs$date<-date(locs$UTC_datetime)
   names(locs)
@@ -64,12 +97,12 @@ for (i in 1:length(prjt)){
   dm_prj<-dm_prj_O %>% 
     pivot_longer(-device_id, names_to = "d_info", values_to = "date")
   
-  
+  dt<-Sys.Date()
   ggplot()+
     geom_point(data=locs, aes(y=as.factor(device_id),x=date), size=0.05)+
     geom_point(data=dm_prj, aes(y=as.factor(device_id),x=date,color=d_info, fill=d_info))+
+    labs(title=dt)+
     ylab("") 
-  ggsave(paste0(usrdir,savedir,"PLOTS/DeploymentCoverage/",prjt[i],"_",dt,"_TimeFrame.png"), dpi=300)
-  
+  ggsave(paste0(usrdir,savedir,"PLOTS/DeploymentCoverage/",prjt[i],"_TimeFrame.png"), dpi=300)
 }
 
