785ae52600caf2d57b8b63c4f39a13607c2f4aa7
Alexa-RoseP <alexa.piggott@oregonstate.edu>
2023-10-31 11:07:22 -0700

    Alexa commit RW coordinate code WIP

--- Bounding_Coordinates_for_RW.R
+++ Bounding_Coordinates_for_RW.R
@@ -1,20 +1,25 @@
 library(data.table) #fread
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 if(Sys.info()[7]=="alexa") {
   usrdir<-"/Users/alexa/Box Sync/DASHCAMS/"
   datadir<-'data/ornitela_for_ATN/'
   savedir<-'Research Workspace/Project Metadata/Bounding Coordinates'
-  deplymatrix<-'data/Field Data/DASHCAMS_Deployment_Field_Data.csv'
+  deplymatrix<-'data/Field Data/Draft_Deployment_Field_Data_single line.csv'
 }
 
 #  Deployment matrix ---------------------------------------------
 deploy_matrix<-read.csv(paste0(usrdir,deplymatrix))
+
+which(is.na(deploy_matrix$DeploymentStartDatetime)) # check for missing deployment start times
+
 deploy_matrix$DeploymentStartDatetime<-mdy_hm(deploy_matrix$DeploymentStartDatetime)-(deploy_matrix$UTC_offset_deploy*60*60)
 deploy_matrix$DeploymentEndDatetime_UTC<-mdy_hm(deploy_matrix$DeploymentEndDatetime_UTC)
 dm<-deploy_matrix%>%select(Bird_ID,TagSerialNumber,Project_ID,Deployment_ID,DeploymentStartDatetime,Deployment_End_Short,DeploymentEndDatetime_UTC,TagManufacture)%>%
   filter(is.na(TagSerialNumber)==FALSE) #selects columns wanted in dm, added deployment ID to my selection
 
-prjt<-unique(dm$Project_ID)
+prjt<-unique(deploy_matrix$Project_ID)
+rm(deploy_matrix)
 prjt<-prjt[prjt!="USACRBRDO14"] #removes MCR 2014
