cd652a8bd2ef40b9a00e6c881b2ca2a1c7405ced
Alexa-RoseP <alexa.piggott@oregonstate.edu>
2023-12-08 20:29:12 -0800

    WIP bounding coordinates by project

--- Bounding_Coordinates_for_RW.R
+++ Bounding_Coordinates_for_RW.R
@@ -56,37 +56,8 @@
   
   locs_wgs84<-st_as_sf(locs,coords=c('lon','lat'),remove = F,crs = 4326)
   
-  #for (i in 1:length(prjt)){
-  
-  # locs<-readRDS(paste0(usrdir,savedir,"Processed_GPS_Deployment_Data/",prjt[i],"_GPS_SpeedFiltered.rds"))
-  
-  #DAT<-NULL
-  #for (j in 1:length(Files)){
-  # dat<-read.csv(file=Files[j],stringsAsFactors=FALSE,sep = ",",fill=TRUE) 
-  #dat<-dat%>%filter(is.na(LatDegree)==FALSE)
-  # dat<-dat%>%select(BirdID,LatDegree,LongDegree,ObsDepth,CalDepth)
-  #DAT<-rbind(DAT,dat)
-  #  }
-  #}
-  
-  # DAT$species<-sapply(strsplit(DAT$BirdID, split='_', fixed=TRUE), function(x) (x[1]))
-  
-  #min(DAT$LatDegree, na.rm=TRUE)
-  #max(DAT$LatDegree, na.rm=TRUE)
-  
-  #min(DAT$LongDegree, na.rm=TRUE)
-  #max(DAT$LongDegree, na.rm=TRUE)
-  
-  max(abs(DAT$CalDepth), na.rm =TRUE)
-  
-  ggplot()+
-    geom_path(data=DAT%>%filter(is.na(LatDegree)==FALSE), 
-              aes(x=LongDegree,y=LatDegree, group=BirdID, color=species))+
-    #facet_wrap(~species)+
-    NULL
-  
-  
-# below needs work, currently gives the lat long for all points not by project!
+
+# Min-max lat/long by project
 
 for (i in 1:length(prjt)){
  pS<-locs_wgs84%>%group_by(Project_ID)%>%
@@ -97,19 +68,18 @@ for (i in 1:length(prjt)){
   
 }
   
+ 
+  
+  head(dm)
+  head(pS)
+ 
+  (dm<-dm%>%group_by(Project_ID,TagSerialNumber)%>%filter(TagManufacture=="Ornitela")%>%
+      mutate(dm_dur=round(difftime(DeploymentEndDatetime_UTC, DeploymentStartDatetime, units="days")), 
+             dm_durD=as.numeric(dm_dur)))   
+  
+ProjBounds<-ps
 
-  psummarise(MINLAT=min(locs_wgs84$lat, na.rm = TRUE),
-            MAXLAT=max(locs_wgs84$lat, na.rm = TRUE),
-            MINLON=min(locs_wgs84$lon, na.rm = TRUE),
-            MAXLON=max(locs_wgs84$lon, na.rm = TRUE))  
-#Bound<-NULL    
- # for (i in 1:length(prjt)){
-    #MINLAT<- min(locs_wgs84$lat, na.rm = TRUE)
-    #MAXLAT<- max(locs_wgs84$lat, na.rm = TRUE)
-    #MINLONG<- min(locs_wgs84$lon, na.rm = TRUE)
-    #MAXLONG<- max(locs_wgs84$lon, na.rm = TRUE)
-#  }  
-   # Bound<-rbind(MINLAT,MAXLAT,MINLONG, MAXLONG)
-  
-  
+
+
+write.csv(pS, paste0(usrdir,savedir,"Bounding_Coordinates_by_Project.csv"))
      
